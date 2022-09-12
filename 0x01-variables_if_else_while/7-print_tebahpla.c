fdfe55eadfe719fd8200906745ea4bb520034ccb
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-12 02:47:18 -0700

    task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /**
- *  * main - program that prints the lowercase in the reverse.
- *   * You can only use the putchar function
- *    * Return: 0
- *     */
-
+* main - program that prints the lowercase in the reverse.
+* You can only use the putchar function
+* Return: 0
+*/
 int main(void)
 {
-	char low;
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-	putchar('\n');
-	return (0);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+return (0);
 }
