5ccc7af7f1eaf4c626b229fce6c5e3a3d3338bbb
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-15 14:04:02 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * print_alphabet in lowercase, followed by a new line.
- * return (0);
- */
+* print_alphabet in lowercase, followed by a new line.
+* return (0);
+*/
 void print_alphabet(void)
 {
 int ch;
