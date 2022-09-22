a2ca13cd6e545cbd26a1ba9158181acc582d24f3
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-21 23:12:09 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 /**                                                             
-* _strlen - returns the length of a string                     
-*
-* @s: string parameter input                                   
-*
-* Return: length of string                                     
+ * _strlen - returns the length of a string                     
+ *
+ * @s: string parameter input                                   
+ *
+ * Return: length of string                                     
 */                                                              
 int _strlen(char *s)                                            
 {                                                               
