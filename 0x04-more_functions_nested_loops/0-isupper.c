ba9ba9206f536a4d8a9d5df4c75e8723dbe7d5ef
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-22 05:37:30 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,13 @@
 * @c: input
 * Return: 1 if c is uppercase, 0 otherwise
 */
-int _isupper(int c)
+int main(void)
 {
-if ((c >= 'A') && (c <= 'Z'))
-return (1);
-else
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
 return (0);
+}
 }
