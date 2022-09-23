db3d4a19f67a423dd72d3ca7e02ec107f7da8add
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-23 03:58:42 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,9 @@
 /**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * _strcmp - a function that compares two strings
- *
- * @s1: string 1 input to compare
- * @s2: against this other string 2
- *
- * Return: 0 if s1 and s2 are equal
- * negative integer if the stopping character
- * in @s1 was less than the stopping
- * character in @s2
- * positive integer if the stopping character
- * in @s1 was greater than the stopping
- * character in @s2
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
