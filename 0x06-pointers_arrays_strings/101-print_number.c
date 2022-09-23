ec027aa168baf00bf36a9440171b8e6ff0c9bfa9
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-23 11:34:01 -0700

    task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,41 @@
 /**
-* print_number - print numbers chars
-* @n: integer params
-* Return: 0
-**/
-void print_number(int n)
+* rot13 - Encodes a string using rot13.
+* @str: The string to be encoded.
+* 
+* Return: A pointer to the encoded string.
+*/
+char *rot13(char *str)
 {
-unsigned int n1;
-n1 = n;
-if (n < 0)
+int indx1 = 0, indx2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S'
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',					
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
+while (str[indx1])
 {
-_putchar('-');
-n1 = -n;
+for (indx2 = 0; indx2 < 52; indx2++)
+{
+if (str[indx1] == alphabet[indx2])
+{
+str[indx1] = rot13key[indx2];
+break;
 }
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
 }
-_putchar((n1 % 10) + '0');
+indx1++;
+}
+return (str);
 }
