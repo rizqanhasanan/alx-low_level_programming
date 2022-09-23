e27fcc630a3223fded553c2e22f1d0ffbd426de4
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-23 04:21:34 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ *string_toupper -  changes all lowercase letters of a string to uppercase
+ * @s: source string.
+ * Return: an uppercase string
+ */
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i])
+{
+s[i] = toupper(s[i]);
+i++;
+}
+return (s);
+}
+
