a2c47f48910463c810d96f9c1c3f547318eebc93
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-23 06:40:36 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,6 @@
 * string_toupper - This function will replace all lowercase letters in
 * the string pointed by str to uppercase
 * @str: The string that will be checked for lowercase letters
-*
 * Return: The resulting string str, where all the letters are uppercase
 */
 char *string_toupper(char *str)
