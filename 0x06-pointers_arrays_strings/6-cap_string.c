5fa83cc1f320128922aa5e0ce7587851dc7ca65f
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-23 07:33:10 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,39 +6,31 @@
 *
 * Return: @s
 */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-int i = 0;
-while (s[i] != '\0')
+int index = 0;
+while (str[index])
 {
-if (s[i] >= 97 && s[i] <= 122)
-if (i == 0)
-{
-s[i] -= 32;							
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||					
+str[index - 1] == '"' ||					    
+str[index - 1] == '(' ||					
+str[index - 1] == ')' ||					
+str[index - 1] == '{' ||					
+str[index - 1] == '}' ||					
+index == 0)			
+str[index] -= 32;
+index++;
 }
-if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
-s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
-s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
-s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
-s[i - 1] == 124)
-{
-s[i] -= 32;
+return (str);
 }
-}
-i++;
-}
-return (s);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
