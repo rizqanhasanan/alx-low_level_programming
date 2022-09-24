0b9d48b1609a9e73aaca70750380e19fd8d58244
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-24 13:20:15 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,12 @@
- /**                                                             
- * _puts - prints a string, followed by a newline to stdout     
- *
- * @str: string parameter to print                              
- *
- * Return: Nothing                                              
- */                                                              
-void _puts(char *str)                                           
-{                                                               
-while (*str != '\0')                                    
-{                                                      
-_putchar(*str + 0);                             
-++str;                                          
-}                                                       
-_putchar('\n');                                         
-}         
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+_putchar('\n');
+}
