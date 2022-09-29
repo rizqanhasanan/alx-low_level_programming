9c2ffa8da328fe18949871c9b9abf6fd429f916f
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-29 15:51:46 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
+     int i = 0;
+
+     for (; n > 0; i++)
 {
-s[i] = b;
-n--;
-}				
-return (s);
+     s[i] = b;
+      n--;
+  }				
+   return (s);
 }
