8bbe0e043f2a299fcdccd9c2ec0661b18a4eae18
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-30 08:17:24 -0700

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 if (*s == 0)
-	return (1);
+return (1);
 return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
@@ -24,8 +24,8 @@ return (check_pal(s, 0, _strlen_recursion(s)));
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
@@ -38,8 +38,8 @@ if (*s == '\0')
 int check_pal(char *s, int i, int len)
 {
 if (*(s + i) != *(s + len - 1))
-	return (0);
-	if (i >= len)
-		return (1);
-	return (check_pal(s, i + 1, len - 1));
+return (0);
+if (i >= len)
+return (1);
+return (check_pal(s, i + 1, len - 1));
 }
