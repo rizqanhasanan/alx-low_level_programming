8e67aae35d63895c23c273dc745470544d6ca2be
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-30 08:24:42 -0700

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,6 +15,7 @@ if (*s == 0)
 return (1);
 return (check_pal(s, 0, _strlen_recursion(s)));
 }
+
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
@@ -27,6 +28,7 @@ if (*s == '\0')
 return (0);
 return (1 + _strlen_recursion(s + 1));
 }
+
 /**
  * check_pal - checks the characters recursively for palindrome
  * @s: string to check
