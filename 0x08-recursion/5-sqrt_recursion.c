ce436af8388967b0e4f6e0c700fff1ebefadd0a0
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-09-30 04:14:03 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,9 +5,11 @@
 *
 * Return: the resulting square root
 */
-int _sqrt_recursion(int n)
+int actual_sqrt_recursion(int n, int i)
 {
-if (n < 0)
+if (i * i > n)
 return (-1);
-return (actual_sqrt_recursion(n, 0));
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
