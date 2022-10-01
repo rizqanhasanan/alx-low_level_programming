f3ed75a8b4b93d1947eba60aa445652596817235
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-10-01 02:47:34 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
 int _atoi(char *s)
 {
 int i, d, n, len, f, digit;
+
 i = 0;
 d = 0;
 n = 0;
@@ -19,6 +20,7 @@ digit = 0;
 
 while (s[len] != '\0')
 len++;
+
 while (i < len && f == 0)
 {
 if (s[i] == '-')
@@ -37,8 +39,10 @@ f = 0;
 }
 i++;
 }
+
 if (f == 0)
 return (0);
+
 return (n);
 }
 
@@ -52,14 +56,18 @@ return (n);
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
+
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
+
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
 result = num1 * num2;
+
 printf("%d\n", result);
+
 return (0);
 }
