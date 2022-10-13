5e71fc0f1a04fbab6a4eb5e68382f365d011adf8
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-10-13 13:53:29 -0700

    tsk0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,7 @@
-#include "function_pointers.h"
-
-/**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
- */
-void print_name(char *name, void (*f)(char *))
-{
-   printf ("arrazy");
-   return 0;
-}
+#ifndef_function_pointers_
+#define_function_pointers_
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array,size_t size, void(*action)(int));
+int int_index(int *array,int size,int(*cmp)(int));
+#endif
