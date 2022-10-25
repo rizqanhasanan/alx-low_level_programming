66a6d9c881a028683b9afe482928bf089fc466d7
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-10-25 04:09:22 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,7 @@
-/*
- * File: 0-print_listint.c
- * Auth: Abdurrazak haruna 
- *    */
-
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
