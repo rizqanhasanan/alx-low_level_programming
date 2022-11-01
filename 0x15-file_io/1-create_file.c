982aaf8539e4c4b2da5efee63779f1f5c4387399
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-11-01 06:12:23 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,71 +1,37 @@
+
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 
 /**
- * create_file - function that creates a file
- * @filename: name of the file to be read
- * @text_content: NULL terminated string to write to the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
+	int o, w, len = 0;
 
-        int fd, write_count, close_status;
-
-	if (filename == NULL)
+        if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-	        fd = open(filename, O_CREAT | O_TRUNC, 0600);
-		if (fd == -1)								     return (-1);
+	        for (len = 0; text_content[len];)
+		       len++;
+	}
 
-		close_status = close(fd);
-		if (close_status == -1)
-			 return (-1);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-		return (1);
-     }
+	if (o == -1 || w == -1)
+	       return (-1);
 
-     fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-     if (fd == -1)
-	     return (-1);
 
-     write_count = write(fd, text_content, _strlen(text_content));
-     if (write_count == -1)
-	              return (-1);
+	close(o);
 
-     close_status = close(fd);
-     if (close_status == -1)
-		       return (-1);
-
-     return (1);
-}
-
-/**
- * _strlen - function that returns the length of a string
- *
- * @s: parameter defined in main
- *
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-        int i = 0;
-
-	while (*s != '\0')
-	{
-	        i++;
-		s++;
-									     }		
-	return (i);
+	return (1);
 }
 
 
